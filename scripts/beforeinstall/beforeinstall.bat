e2604abdbc8fca7aa3f0a43f528e12c44cbf52e2
Sharma, Anuj <Anuj.Sharma@exchange.nordstrom.com>
2016-02-12 12:19:38 -0800

    Correcting path in bat file

--- scripts/beforeinstall/beforeinstall.bat
+++ scripts/beforeinstall/beforeinstall.bat
@@ -1,3 +1,3 @@
 SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%before-install.ps1
+SET PowerShellScriptPath=%ThisScriptsDirectory%beforeinstall.ps1
 %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command "& '%PowerShellScriptPath%'"
