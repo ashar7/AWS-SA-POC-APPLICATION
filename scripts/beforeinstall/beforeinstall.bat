ce11ea3768a9ed9808bdcaa0657b00f2ca6358b2
Sharma, Anuj <Anuj.Sharma@exchange.nordstrom.com>
2016-02-12 12:28:20 -0800

    Setting execution policy

--- scripts/beforeinstall/beforeinstall.bat
+++ scripts/beforeinstall/beforeinstall.bat
@@ -1,3 +1,3 @@
 SET ThisScriptsDirectory=%~dp0
 SET PowerShellScriptPath=%ThisScriptsDirectory%beforeinstall.ps1
-%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -NoLogo -NonInteractive -NoProfile -command "& '%PowerShellScriptPath%'"
+%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -NoLogo -NonInteractive -NoProfile -ExecutionPolicy Bypass -Scope LocalMachine -command "& '%PowerShellScriptPath%'"
