e1bed378367b7a743b393bb21624694c26325e7c
Sharma, Anuj <Anuj.Sharma@exchange.nordstrom.com>
2016-02-12 12:22:35 -0800

    Bypass local script run execution policy

--- scripts/beforeinstall/beforeinstall.ps1
+++ scripts/beforeinstall/beforeinstall.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine
 Install-WindowsFeature Web-Server
 Import-Module WebAdministration
 Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value d:\inetpub\wwwroot
