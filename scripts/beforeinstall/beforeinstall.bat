6d1af5fa65bb9c632b0bbc61cc87f50b8f496fb9
Sharma, Anuj <Anuj.Sharma@exchange.nordstrom.com>
2016-02-12 16:16:32 -0800

    Adding noexit

--- scripts/beforeinstall/beforeinstall.bat
+++ scripts/beforeinstall/beforeinstall.bat
@@ -1,3 +1,3 @@
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module WebAdministration
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value d:\inetpub\wwwroot
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -noexit -Command Install-WindowsFeature Web-Server
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -noexit -Command Import-Module WebAdministration
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -noexit -Command Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value d:\inetpub\wwwroot
