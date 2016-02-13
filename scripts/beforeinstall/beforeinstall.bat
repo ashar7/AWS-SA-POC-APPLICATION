8933009ca7a22262256b7f39cf85fd8684cfb57d
Sharma, Anuj <Anuj.Sharma@exchange.nordstrom.com>
2016-02-12 16:04:56 -0800

    Moving the commands to bat file

--- scripts/beforeinstall/beforeinstall.bat
+++ scripts/beforeinstall/beforeinstall.bat
@@ -1,3 +1,3 @@
-SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%beforeinstall.ps1
-%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -NoLogo -NonInteractive -NoProfile -ExecutionPolicy Bypass -Scope LocalMachine -command "& '%PowerShellScriptPath%'"
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module WebAdministration
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value d:\inetpub\wwwroot
